0613b3de4bbf490872fdde7c594b3c58927c1b35
hasinirajasinghe <hasinichathu@gmail.com>
2022-04-29 00:00:51 -0400

    Added funtionality to the second view (levels choice) and added majority of questions

--- questions-and-answers.js
+++ questions-and-answers.js
@@ -27,3 +27,143 @@ const grade1 = [
         choices: ["5","12","6","3"]
     },
 ]
+
+const grade2 = [
+    {
+        question: "What does a terre mean?",
+        correctAnswer: "On the ground ",
+        choices: ["On the ground", "In the air", "To be lifted", "None"]
+    },
+    {
+        question: "What is Demi Detourne?",
+        correctAnswer: "Half turn",
+        choices: ["Half turn", "Full turn", "Linked turn", "Double turn "]
+    },
+    {
+        question: "What is a Chagement?",
+        correctAnswer: "A spring from 5th to 5th position, changing feet",
+        choices: ["A spring from 5th to 5th position, changing feet", "A spring from 3rd to 3rd position, changing feet", "A spring from 1st to 1st position, changing feet", "A spring from 4th to 4th position, changing feet"]
+    },
+    {
+        question: "What are extreme ends of Rond de Jambe a terre?",
+        correctAnswer: "4th position, devant, derriere",
+        choices: ["4th position, devant, derriere", "5th position, devant, derriere", "2nd position, devant, derriere", "3rd position, devant, derriere"]
+    },
+    {
+        question: "",
+        correctAnswer: "",
+        choices: ["", "", "", ""]
+    },
+]
+
+const grade3 = [
+    {
+        question: "How many positions of the feet are there?",
+        correctAnswer: "5",
+        choices: ["5", "4", "6", "12"]
+    },
+    {
+        question: "How should the weight of the body be distributed on the feet",
+        correctAnswer: "All three (Big toe, Little toe and Heel)",
+        choices: ["All three (Big toe, Little toe and Heel)", "Big toe", "Little toe", "Heel"]
+    },
+    {
+        question: "What does pirouette mean?",
+        correctAnswer: "To turn",
+        choices: ["To turn", "To spring", "To jump", "To leap"]
+    },
+    {
+        question: "How many ways can you turn?",
+        correctAnswer: "Endehors and Endedans",
+        choices: ["Endehors and Endedans", "Endehors", "Endedans", ""]
+    },
+    {
+        question: "What is grand battement?",
+        correctAnswer: "A throwing of the leg into the air",
+        choices: ["A throwing of the leg into the air", "The step of the cat", "Beating and striking", "Unfolding of the led into the air"]
+    },
+]
+
+const grade4 = [
+    {
+        question: "What does pique mean?",
+        correctAnswer: "Pick up sharply",
+        choices: ["Pick up sharply", "Small beating", "To snatch", "To point"]
+    },
+    {
+        question: "What does petit battement mean?",
+        correctAnswer: "Small beating",
+        choices: ["Small beating", "Beating", "Beating and Striking", "Striking"]
+    },
+    {
+        question: "What is a glissade?",
+        correctAnswer: "A step and a glide",
+        choices: ["A step and a glide", "A step", "A glide", "A spring"]
+    },
+    {
+        question: "Is an arabesque terre or enl’/air?",
+        correctAnswer: "Either",
+        choices: ["Either", "Terre", "Enl’/air", "Neither "]
+    },
+    {
+        question: "How many steps are in Pas de Bourree?",
+        correctAnswer: "3",
+        choices: ["3", "4", "2", "5"]
+    },
+]
+
+const grade5 = [
+    {
+        question: "What is the meaning of coupe, fouette, raccourci",
+        correctAnswer: "To cut, whip and return",
+        choices: ["To cut, whip and return", "To step, spring, and return", " To whip, cut and return", "To  spring, step and return"]
+    },
+    {
+        question: "What does serre mean?",
+        correctAnswer: "Continuous",
+        choices: ["Continuous", "Backwards", "Gliding", "Balance"]
+    },
+    {
+        question: "What does tombe mean?",
+        correctAnswer: "To fall or drop on to",
+        choices: ["To fall or drop on to", "To strike", "To glide", "A soft step"]
+    },
+    {
+        question: "What is meant by petit retires?",
+        correctAnswer: "Small retire movements ",
+        choices: ["Small retire movements", "A step of the cat", "Retire movements", "Big retire movements"]
+    },
+    {
+        question: "What is fouette battement?",
+        correctAnswer: "A circular grand battement from 5th to 5th",
+        choices: ["A circular grand battement from 5th to 5th", "A circular grand battement from 4th to 4th ", "A circular grand battement from 3rd to 3rd", "A circular grand battement from 1st to 1st"]
+    },
+]
+
+const majorGrades = [
+    {
+        question: "What is the difference between a Battement Tendu and a Glisse?",
+        correctAnswer: "A battement tendu does not leave the floor",
+        choices: ["A battement tendu does not leave the floor", "A battement glisse does not leave the floor", "A battement tendu does  leave the floor", "A battement glisse does  leave the floor"]
+    },
+    {
+        question: "How many types of releves are there?",
+        correctAnswer: "2",
+        choices: ["2", "4", "5", "6"]
+    },
+    {
+        question: "What are chaines?",
+        correctAnswer: "linked half turns",
+        choices: ["linked half turns", "linked full turns", "whipped turn", "turn on one leg"]
+    },
+    {
+        question: "What is a Pas Marche?",
+        correctAnswer: "A walking step",
+        choices: ["A walking step", "A gliding step", "Broken step", "A spring step"]
+    },
+    {
+        question: "",
+        correctAnswer: "",
+        choices: ["", "", "", ""]
+    },
+]
--- script.js
+++ script.js
@@ -7,9 +7,28 @@
     //Else if How to Play is clicked:
         //A pop up of some sort should show the instructions for the game
 
+//2nd View 
+//Contains the levels View 
+    //If player clicks on one out of the six levels, prompt the first question from the radomized list of questions.
+
+//3rd View and onwards 
+//Questions and answers view
+    //Display one question at a time with four choices for the chosen level
+        //If player clicks on a choice
+            //If player choice === correct choice
+                //Increment score by 1    
+            //Move on to the next question 
+    
+//Last View 
+//Contains results from each round 
+    //Will show the final score of the player
+    //Will which questions were correctly answered and incorrectly answered 
+    //Show a restart button to take the player back to the main page 
+
+let questionSet;    
 let startButton = document.getElementById('start');
+let mainContainer = document.getElementById('main-container');
 startButton.addEventListener('click', () => {
-    let mainContainer = document.getElementById('main-container');
     //Clears the initial view of the game 
     mainContainer.replaceChildren();
     //Begining of creating the second view of different levels in the game
@@ -30,7 +49,53 @@ startButton.addEventListener('click', () => {
     levelNames.forEach((levelName) => {
         let newLevelButton = document.createElement('button');
         newLevelButton.innerHTML = levelName;
+        newLevelButton.addEventListener('click', levelSelect)
         mainContainer.append(newLevelButton);
-        //Create a new style class to format the buttons 
-    })
+        //TODO: Create a new style class to format the buttons
+    }) 
 })
+
+// When one of the level buttons is clicked this fuction is called on
+function levelSelect(e) {
+    let level = e.target;
+    // Load the appropriate question based the button that was clicked and store in the global variable
+    switch (level.innerHTML) {
+        case 'Grade 1':
+            questionSet = grade1;
+            break;
+        case 'Grade 2':
+            questionSet = grade2;
+            break;
+        case 'Grade 3':
+            questionSet = grade3;
+            break;
+        case 'Grade 4':
+            questionSet = grade4;
+            break;
+        case 'Grade 5':
+            questionSet = grade5;
+            break;
+        case 'Major Grades':
+            questionSet = majorGrades;
+            break;        
+    }
+
+    // Clears the levels view of the game
+    mainContainer.replaceChildren();
+    // Creates the neccessary divs to store questions and choices
+    let question = document.createElement('div');
+    question.id = "question"
+    let choicesContainer = document.createElement('div');
+    choicesContainer.id = "choices-container"
+    let choice1 = document.createElement('div');
+    choice1.id = 'choice1'
+    let choice2 = document.createElement('div');
+    choice2.id = 'choice2'
+    let choice3 = document.createElement('div');
+    choice3.id = 'choice3'
+    let choice4 = document.createElement('div');
+    choice4.id = 'choice4'
+    // Adding the newly created questions and choices to the main container
+    mainContainer.append(question, choicesContainer)
+    choicesContainer.append(choice1, choice2, choice3, choice4)
+}
